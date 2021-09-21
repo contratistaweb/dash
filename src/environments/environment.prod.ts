d6ab9efb605732a10b92388a39e9a5fa89930ae4
Luillin A. Escobar <luillin.escobar@hotmail.com>
2021-09-21 08:21:20 -0500

    env produccion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
 export const environment = {
-  production: true
+  production: true,
+  API: 'https://cinemax-32472-default-rtdb.firebaseio.com',
+  urlDB: 'https://dash-proyect-default-rtdb.firebaseio.com',
+  firebaseConfig: {
+    apiKey: "AIzaSyCXvPT77Wnqv8BTmlGprBJeXOhODeL5d28",
+    authDomain: "cinemax-32472.firebaseapp.com",
+    databaseURL: "https://cinemax-32472-default-rtdb.firebaseio.com",
+    projectId: "cinemax-32472",
+    storageBucket: "cinemax-32472.appspot.com",
+    messagingSenderId: "135029741160",
+    appId: "1:135029741160:web:b6271a9ce55ee1f32c4ddd",
+    measurementId: "G-W544PD20ML"
+  },
+  unplash: {
+    url: 'https://api.unsplash.com',
+    accesskey: 'wu8eoz0D0L9KavouaQ1Fs8wakVF2wjI0TSGiHvJAxhc'
+  }
 };
